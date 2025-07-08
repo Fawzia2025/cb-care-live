fa018c4188a87f78927d372e78a8b41e55d3c14c
Hasan Murtaza <hasanmurtaza88@gmail.com>
2025-07-08 16:16:53 +0500

    added config file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx"; // Updated to import from App.jsx
-import "./index.css"; // This imports your base CSS (where Tailwind directives are)
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all JS/JSX files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
